5e3a78a1f08570ab6bec96379b653e2ed2d95fe4
Student <student@Techs-MacBook-Air.local>
2021-02-16 19:59:57 -0500

    3rd push

--- 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.ProjectDAO;
 
@@ -20,17 +22,43 @@ public class JDBCProjectDAO implements ProjectDAO {
 	
 	@Override
 	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
+
+		ArrayList<Project> projects = new ArrayList<>();
+		String sqlgetAllActiveProjects = "SELECT project_id name, from_date, to_date"+
+										   "FROM project ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlgetAllActiveProjects);
+		while(results.next()) {
+			Project theProject = mapRowToProject(results);
+			projects.add(theProject);
+		}
+		return projects;
 	}
+		
+		
 
 	@Override
 	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
+		String sql = "UPDATE employee set project_id = null"+
+				"JOIN project_employee ON employee.employee_id = project_employee.employee_id"+
+				"WHERE employee_id =?";
+jdbcTemplate.update("UPDATE employee set project_id WHERE employee_id = ?", employeeId, projectId); 
 		
+			
 	}
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
-		
+		String sql = "UPDATE employee set project_id = ?"+
+				"JOIN project_employee ON employee.employee_id = project_employee.employee_id"+
+				"WHERE employee_id =?";
+jdbcTemplate.update("UPDATE employee set project_id WHERE employee_id = ?", employeeId, projectId); 
 	}
 
+	private Project mapRowToProject(SqlRowSet results) {
+		Project theProject;
+		theProject = new Project();
+		return theProject;
+	}
+	
+	
 }
